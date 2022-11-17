dec3e9e47a0d49fe843f9bd3d0d959e23efdf1b3
Jesica Biecher <j.biecher@fvfj92d21wgc.speedport.ip>
2022-11-17 16:17:11 +0100

    getPlayerSelection Static

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,24 @@
+// Computer Play
 function getComputerChoice() {
     const things = ['Rock', 'Paper', 'Scissor'];
     const thing = things[Math.floor(Math.random() * things.length)];
-    alert('The computer chose:' + thing);
+    alert('The computer chose: ' + thing);
+    return thing;
 }
 
-console.log(getComputerChoice());
+// Player Temp Play
+function getPlayerSelection() {
+    const things = ['Rock', 'Paper', 'Scissor'];
+    const thing = things[Math.floor(Math.random() * things.length)];
+    alert('The player chose:' + thing);
+    return thing;
+}
+
+// Rounds
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+}
+
+const playerSelection = getPlayerSelection();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
