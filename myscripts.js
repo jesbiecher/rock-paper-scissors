534f19638fae9d2e1a6460712bd2357de525521f
Jesica Biecher <j.biecher@fvfj92d21wgc.speedport.ip>
2022-11-21 11:21:55 +0100

    Final removing comments

--- myscripts.js
+++ myscripts.js
@@ -13,21 +13,20 @@ function game() {
         console.log('End of Round: ' + round + ': Player Score: ' + playerScore + '. Computer Score: ' + computerScore);
     }
     if (playerScore > computerScore) {
-        console.log('You Win!');
+        alert('You Win!');
         return 'Player Wins';
     } else {
-        console.log('You Lose!');
+        alert('You Lose!');
         return 'Computer Wins';
     }
 }
 
-console.log(game() + '. Game Result \(Round ' + round + '\): Player Score: ' + playerScore + '. Computer Score: ' + computerScore);
+console.log(game() + '. Game Result: Player Score: ' + playerScore + '. Computer Score: ' + computerScore);
 
 // Computer Play
 function getComputerChoice() {
     const things = ['Rock', 'Paper', 'Scissors'];
     const thing = things[Math.floor(Math.random() * things.length)];
-    // alert('The computer chose: ' + thing);
     return thing;
 }
 
@@ -38,13 +37,10 @@ function getPlayerSelection() {
         var thing = prompt('Choose Rock Paper or Scissors');
         switch (capitalizeFirstLetter(thing)) {
             case "Rock":
-                // alert('The player chose: ' + capitalizeFirstLetter(thing));
                 break loop;
             case "Paper":
-                // alert('The player chose: ' + capitalizeFirstLetter(thing));
                 break loop;
             case "Scissors":
-                // alert('The player chose: ' + capitalizeFirstLetter(thing));
                 break loop;
             default: alert("Please enter a valid value");
         }
@@ -65,42 +61,24 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == 'Rock') {
         if (computerSelection == 'Paper') {
-            // alert('You Lose! Paper beats Rock');
             computerScore++;
-            // return ('Computer Wins');
         } else if (computerSelection == 'Scissors') {
-            // alert('You Win! Rock beats Scissors');
             playerScore++;
-            // return ('You win!');
         } else if (computerSelection == 'Rock') {
-            // alert('It\'s a tie');
-            // return ('It\'s a tie');
         }
     } else if (playerSelection == 'Paper') {
         if (computerSelection == 'Paper') {
-            // alert('It\'s a tie');
-            // return ('It\'s a tie');
         } else if (computerSelection == 'Scissors') {
-            // alert('You Lose! Rock beats Scissors');
             computerScore++;
-            // return ('Computer Wins');
         } else if (computerSelection == 'Rock') {
-            // alert('You Win! Paper beats Rock');
             playerScore++;
-            // return ('You win!');
         }
     } else if (playerSelection == 'Scissors') {
         if (computerSelection == 'Paper') {
-            // alert('You Win! Rock beats Scissors');
             playerScore++;
-            // return ('You win!');
         } else if (computerSelection == 'Scissors') {
-            // alert('It\'s a tie');
-            // return ('It\'s a tie');
         } else if (computerSelection == 'Rock') {
-            // alert('You Lose! Rock beats Scissors');
             computerScore++;
-            // return ('Computer Wins');
         }
     }
 }
