8221e253e84a29e9a9262e564326c17cf140793e
Jesica Biecher <j.biecher@fvfj92d21wgc.speedport.ip>
2022-12-01 11:31:42 +0100

    UI and new js

--- myscripts.js
+++ myscripts.js
@@ -1,51 +1,65 @@
 // Variables
-let thing = '';
-let round = 0;
+const gameEl = document.querySelector('.gameButton');
+const gameScreenEl = document.querySelector('.gameScreen');
+const mainScreenEl = document.querySelector('.mainScreen');
 
+let round = 0;
 let computerScore = 0;
 let playerScore = 0;
 
+const playerPointsEl = document.getElementById('player-score');
+const computerPointsEl = document.getElementById('computer-score');
+
+const playerChoiceEl = document.getElementById('player-choice');
+const computerChoiceEl = document.getElementById('computer-choice');
+
+const resultLogEl = document.getElementById('result-log');
+const winnerEL = document.getElementById('winner');
+const gameResultsEl = document.getElementById('gameResults');
+
+// Event Listeners
+gameEl.addEventListener('click', startGame);
+
+let playerChoices = document.querySelectorAll('.button');
+playerChoices.forEach((playerChoice) => {
+    playerChoice.addEventListener('click', () => {
+        playerSelection = capitalizeFirstLetter(playerChoice.id);
+        computerSelection = getComputerSelection();
+        playRound(playerSelection, computerSelection);
+    });
+});
+
+// Functions
+function startGame() {
+    console.log("Clicked inside testGame gameButton");
+    gameScreenEl.style.display = 'flex';
+    mainScreenEl.style.display = 'none';
+}
+
 // Game = Set of 5 Rounds
-function game() {
-    while (round < 5) {
-        playRound(getPlayerSelection(), getComputerChoice());
-        round++;
-        console.log('End of Round: ' + round + ': Player Score: ' + playerScore + '. Computer Score: ' + computerScore);
-    }
-    if (playerScore > computerScore) {
-        alert('You Win!');
-        return 'Player Wins';
-    } else {
-        alert('You Lose!');
-        return 'Computer Wins';
+function playGame() {
+    if (round === 5) {
+        if (playerScore > computerScore) {
+            winnerEL.textContent = 'You Win!';
+            console.log('**** End GAME');
+            return stopGame();
+        } else if (computerScore > playerScore) {
+            winnerEL.textContent = 'You Lose!';
+            console.log('**** End GAME');
+            return stopGame();
+        } else {
+            winnerEL.textContent = 'It\'s a tie!';
+            console.log('**** End GAME');
+            return stopGame();
+        }
     }
 }
 
-console.log(game() + '. Game Result: Player Score: ' + playerScore + '. Computer Score: ' + computerScore);
-
 // Computer Play
-function getComputerChoice() {
+function getComputerSelection() {
     const things = ['Rock', 'Paper', 'Scissors'];
     const thing = things[Math.floor(Math.random() * things.length)];
     return thing;
-}
-
-// Player Play Prompt
-function getPlayerSelection() {
-    loop:
-    while (true) {
-        var thing = prompt('Choose Rock Paper or Scissors');
-        switch (capitalizeFirstLetter(thing)) {
-            case "Rock":
-                break loop;
-            case "Paper":
-                break loop;
-            case "Scissors":
-                break loop;
-            default: alert("Please enter a valid value");
-        }
-    }
-    return capitalizeFirstLetter(thing);
 }
 
 // Capitalize
@@ -56,9 +70,8 @@ function capitalizeFirstLetter(s) {
 
 // Round
 function playRound(playerSelection, computerSelection) {
-    console.log('Player Selection: ' + playerSelection);
-    console.log('Computer Selection: ' + computerSelection);
-
+    round++;
+    console.log('**** ROUND ' + round);
     if (playerSelection == 'Rock') {
         if (computerSelection == 'Paper') {
             computerScore++;
@@ -81,4 +94,17 @@ function playRound(playerSelection, computerSelection) {
             computerScore++;
         }
     }
+    playerChoiceEl.textContent = 'You chose: ' + `${playerSelection}`;
+    computerChoiceEl.textContent = 'Computer chose: ' + `${computerSelection}`;
+    playerPointsEl.textContent = 'Player Score: ' + playerScore;
+    computerPointsEl.textContent = 'Computer Score: ' + computerScore;
+
+    return playGame(playerSelection, computerSelection);
+}
+
+function stopGame() {
+    playerChoices.forEach(button => {
+        button.disabled = true;
+    });
+    gameResultsEl.style.visibility = 'visible';
 }
